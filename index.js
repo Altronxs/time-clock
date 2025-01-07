1ee757028d17600da57a3565ad3269fd842ab00e
Altronxs <kylextron101098@gmail.com>
2025-01-07 11:09:17 -1000

    add fixes

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-$(document).ready(function() {
-    function displayTime() {
-        var currentTime = new Date();
-        var hours = currentTime.getHours();
-        var minutes = currentTime.getMinutes();
-        var seconds = currentTime.getSeconds();
 
-        var clockDiv = document.getElementById('clock');
-        clockDiv.innerText = hours + ":" + minutes + ":" + seconds;
-    }
+function displayTime() {
+    var currentTime = new Date();
+    var hours = currentTime.getHours();
+    var minutes = currentTime.getMinutes();
+    var seconds = currentTime.getSeconds();
 
-    displayTime();
-    setInterval(displayTime, 1000);
-});
+    var clockDiv = document.getElementById('clock');
+    clockDiv.innerText = hours + ":" + minutes + ":" + seconds;
+    console.log( hours + ":" + minutes + ":" + seconds)
+}
+
+displayTime();
+setInterval(displayTime, 1000);
 
