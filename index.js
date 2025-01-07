65dca97a8c609e66079c78576ac9627df50184d1
Altronxs <kylextron101098@gmail.com>
2025-01-07 11:07:04 -1000

    add fixes

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ $(document).ready(function() {
         var hours = currentTime.getHours();
         var minutes = currentTime.getMinutes();
         var seconds = currentTime.getSeconds();
-        console.log(hours,minutes,seconds);
+
+        var clockDiv = document.getElementById('clock');
+        clockDiv.innerText = hours + ":" + minutes + ":" + seconds;
     }
+
+    displayTime();
+    setInterval(displayTime, 1000);
 });
 
-var currentTime = new Date();
-        var hours = currentTime.getHours();
-        var minutes = currentTime.getMinutes();
-        var seconds = currentTime.getSeconds();
-        console.log(hours,minutes,seconds);
